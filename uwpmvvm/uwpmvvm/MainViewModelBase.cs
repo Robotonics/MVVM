24e9fa97f55cb441446cf1cf225c34cf6c6ba8f8
robotonics <roboshopz@gmail.com>
2016-02-11 21:02:28 +0000

    Completed Calculator App

---
+++ uwpmvvm/uwpmvvm/MainViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace uwpmvvm
+{
+    
+        public abstract class MainViewModelBase : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChangedEventHandler handler = this.PropertyChanged;
+                if (handler != null)
+                {
+                    handler(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+
+
+    
+}
